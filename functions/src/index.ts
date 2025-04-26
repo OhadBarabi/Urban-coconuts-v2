f03471f908ebbbc0f7d6efd11911d587efbd4e71
obarlaw <obarlaw@gmail.com>
2025-04-26 13:14:32 +0000

    Success 19: Initial project setup with v5 complete (models, index files created, build successful)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,5 @@
 import * as admin from 'firebase-admin';
-import { helloWorld } from './core/helloWorld';
-import { onUserCreate } from './auth/onUserCreate';
-import { getUserProfile } from "./users/getUserProfile";
 
 admin.initializeApp();
 
-console.log("Firebase Admin SDK initialized successfully.");
-
-
-export { helloWorld, onUserCreate, getUserProfile };
+console.log('Firebase Admin SDK initialized successfully.');
